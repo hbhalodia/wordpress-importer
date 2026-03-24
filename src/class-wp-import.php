244e6273b674d0dca65283335755eb4be4c56d88
hbhalodia <hit.bhalodia@rtcamp.com>
2026-03-24 22:42:25 +0530

    Revert condition check for attribute_start

--- src/class-wp-import.php
+++ src/class-wp-import.php
@@ -1199,7 +1199,7 @@ class WP_Import extends WP_Importer {
 			$attribute_json_start = strcspn( $attribute_string, '{' );
 			$attribute_json_end   = strrpos( $attribute_string, '}' );
 
-			if ( false === $attribute_json_start || false === $attribute_json_end || $attribute_json_start >= $attribute_json_end ) {
+			if ( false === $attribute_json_end || $attribute_json_start >= $attribute_json_end ) {
 				continue;
 			}
 
